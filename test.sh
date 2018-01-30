b17bc7b2e39eac475eb588970c886aa1e7cbe004
Antony Cartwright <antonycc@users.noreply.github.com>
2018-01-30 21:31:48 +0100

    Updated test for PDF as input

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Given an archive of multiple files to process"
+echo "Given a pdf to process"
 aws s3 rm "s3://serverless-wordcount-hopper/" --recursive
 aws s3 rm "s3://serverless-wordcount-result/" --recursive
 aws s3 rm "s3://serverless-wordcount-archive/" --recursive
-aws s3 cp "fragments-to-process-1K.tar.gz" "s3://serverless-wordcount-hopper/"
+aws s3 cp "pd-to-process.pdf" "s3://serverless-wordcount-hopper/"
 
-echo "When the archive is placed in a bucket"...
+echo "When the pdf is placed in a bucket"...
 aws s3 ls "s3://serverless-wordcount-hopper/"
 
-echo "Then the files in the archive are examined"
+echo "Then the fragments in the pdf are examined"
 echo "and the words in the sentence fragments are counted in a dataframe"
 echo "and the data frame is exported to a configured target folder"...
 echo -n "." ; sleep 1 
